380f5154639a5217782286ee3747b0e4449201ba
JosephOlabode <josepholabode17@gmail.com>
2021-01-30 11:58:31 +0000

    implemented the test code for the second and third example

--- test/routes/rule-validation.test.js
+++ test/routes/rule-validation.test.js
@@ -60,8 +60,62 @@ describe('rule validation routes', () => {
             })
         });
 
-        it('', async () => {
+        it('should return success with a message field missions.count successfully validated.', async () => {
+            const data = {
+                "rule": {
+                    "field": "missions.count",
+                    "condition": "gte",
+                    "condition_value": 30
+                },
+                "data": {
+                    "name": "James Holden",
+                    "crew": "Rocinante",
+                    "age": 34,
+                    "position": "Captain",
+                    "missions": {
+                        "count": 45,
+                        "successful": 44,
+                        "failed": 1
+                    }
+                }
+            }
 
-        })
+            const res = await request.post(`/validate-rule`).send(data);
+            expect(res.status).toBe(200);
+            expect(res.body.message).toBe(`field ${data.rule.field} successfully validated.`);
+            expect(res.body.data).toMatchObject({
+                validation: {
+                    error: "false",
+                    field: data.rule.field,
+                    field_value: data.data.missions.count,
+                    condition: data.rule.condition,
+                    condition_value: data.rule.condition_value
+                }
+            })
+        });
+
+        it('should return success with a message field 0 failed validation.', async () => {
+            const data = {
+                "rule": {
+                    "field": "0",
+                    "condition": "eq",
+                    "condition_value": "a"
+                },
+                "data": "damien-marley"
+            }
+
+            const res = await request.post(`/validate-rule`).send(data);
+            expect(res.status).toBe(400);
+            expect(res.body.message).toBe(`field ${data.rule.field} failed validation.`);
+            expect(res.body.data).toMatchObject({
+                validation: {
+                    error: "true",
+                    field: data.rule.field,
+                    field_value: data.data[parseInt(data.rule.field)],
+                    condition: data.rule.condition,
+                    condition_value: data.rule.condition_value
+                }
+            })
+        });
     })
 })
